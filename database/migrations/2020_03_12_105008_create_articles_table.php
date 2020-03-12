76db7af636d43a8d3303dcd908b310ce65e3312b
Aleem Isiaka <aleemisiaka@gmail.com>
2020-03-12 11:58:16 +0100

    Migrations for author schema

---
+++ database/migrations/2020_03_12_105008_create_articles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('author_name');
+            $table->integer('author_id');
+            $table->string('title');
+            $table->string('body')->min(10);
+            $table->timestamps();
+
+            $table->foreign('author_id')->references('id')->on('users')->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign('articles_author_id_foreign');
+        });
+
+        Schema::dropIfExists('articles');
+    }
+}
