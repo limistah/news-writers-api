59f8384c8d47595f947f2bbc0f02fbd01b9063ea
Aleem Isiaka <aleemisiaka@gmail.com>
2020-03-12 23:36:45 +0100

    Completes loading an article by a writer

--- app/Http/Controllers/UsersArticlesController.php
+++ app/Http/Controllers/UsersArticlesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
+use App\Http\Resources\ArticlesResource;
+use App\User;
 use Illuminate\Http\Request;
 
 class UsersArticlesController extends Controller
@@ -12,8 +15,8 @@ class UsersArticlesController extends Controller
      * @param  int  $writer
      * @return \Illuminate\Http\Response
      */
-    public function show($writer)
+    public function show(User $writer)
     {
-        return response()->json(["message" => "Returns an article by just a writer"]);
+        return ArticlesResource::collection(Article::with("author")->where("author_id", "=", $writer->id)->paginate(5));
     }
 }
--- app/Http/Resources/WritersResource.php
+++ app/Http/Resources/WritersResource.php
@@ -15,6 +15,7 @@ class WritersResource extends JsonResource
     public function toArray($request)
     {
         return [
+            "id" => $this->id,
             "name" => $this->name,
             "email" => $this->email,
             "bio" => $this->bio,
